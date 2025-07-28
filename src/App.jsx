4201ae24b2d765c0b4f817f0f93bde58937f32dd
R-Htu <htuaung89@gmail.com>
2025-07-28 21:34:58 +0630

    Add React + Vite project files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React with Vite!</h1>
+      <p>Welcome to your first React App.</p>
+    </div>
+  )
+}
+export default App
+
